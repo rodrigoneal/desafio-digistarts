4f9b116ca50e8e1848f48be8f9451aef03c4aad9
rodrigoneal <rodrigho2006@gmail.com>
2021-01-15 19:11:57 -0300

    Criando metodo estatico para retornar o valor já formatado

--- desafio_binario/binary_calculator.py
+++ desafio_binario/binary_calculator.py
@@ -2,37 +2,32 @@ class BinaryCalculator:
 
     def __init__(self, value):
         self.value = int(value, 2)
+        self.result = 0
+
+    @staticmethod
+    def _verbose(value):
+        size = 8 - len(value) + 2 if len(value) < 8 else None
+        if size:
+            convert = value.replace('0b', '0' * size)
+            return convert
 
     def __add__(self, other):
-        result = bin(self.value + other.value)
-        size = 8 - len(result) + 2 if len(result) < 8 else None
-        if size:
-            result = result.replace('0b', '0' * size)
-        return result
+        sum = bin(self.value + other.value)
+        return self._verbose(sum)
 
     def __sub__(self, other):
-        result = bin(self.value - other.value)
-        size = 8 - len(result) + 2 if len(result) < 8 else None
-        if size:
-            result = result.replace('0b', '0' * size)
-        return result
+        sub = bin(self.value - other.value)
+        return self._verbose(sub)
 
     def __mul__(self, other):
-        result = bin(self.value * other.value)
-        size = 8 - len(result) + 2 if len(result) < 8 else None
-        if size:
-            result = result.replace('0b', '0' * size)
-        return result
+        mul = bin(self.value * other.value)
+        return self._verbose(mul)
+
 
     def __mod__(self, other):
-        result = bin(self.value % other.value)
-        size = 8 - len(result) + 2 if len(result) < 8 else None
-        if size:
-            result = result.replace('0b', '0' * size)
-        return result
+        mod = bin(self.value % other.value)
+        return self._verbose(mod)
+
 
     def __repr__(self):
-        return f'BinaryCalculator({bin(self.value)[2:0]})'
-
-
-
+        return f'BinaryCalculator({bin(self.value)[2:]})'
