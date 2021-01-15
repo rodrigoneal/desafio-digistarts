d3a4666f8d5dc173964162531a4097c43a9945d9
rodrigoneal <rodrigho2006@gmail.com>
2021-01-15 18:41:22 -0300

    Testes com a classe BinaryCalculator com os valores passados no email

--- desafio_binario/test/test_calculator_bin.py
+++ desafio_binario/test/test_calculator_bin.py
@@ -4,20 +4,17 @@ from desafio_binario.binary_calculator import BinaryCalculator
 
 class TestBinaryCalculator(unittest.TestCase):
 
-    def test_sum_return_bin_abre(self):
-        self.assertEqual(BinaryCalculator('00000001', '00000011').sum_binary(), '0b100')
+    def test_sum_binary_calculator(self):
+        self.assertEqual(BinaryCalculator('00000001') + BinaryCalculator('00000011'), '00000100')
 
-    def test_sum_verbose_return_bin_complete(self):
-        self.assertEqual(BinaryCalculator('00000001', '00000011').sum_binary(verbose=True), '00000100')
+    def test_sub_binary_calculator(self):
+        self.assertEqual(BinaryCalculator('00000010') - BinaryCalculator('00000001'), '00000001')
 
-    def test_sub_return_abre(self):
-        self.assertEqual(BinaryCalculator('00000010', '00000001').sub_binary(), '0b1')
+    def test_mul_binary_calculator(self):
+        self.assertEqual(BinaryCalculator('00000001') * BinaryCalculator('00000011'), '00000011')
 
-    def test_sub_verbose_return_bin_complete(self):
-        self.assertEqual(BinaryCalculator('00000010', '00000001').sub_binary(verbose=True), '00000001')
-
-    def test_mul_return_abre(self):
-        self.assertEqual(BinaryCalculator('00000010', '00000001').mul_binary(), '00000001')
+    def test_mod_binary_calculator(self):
+        self.assertEqual(BinaryCalculator('00010100') % BinaryCalculator('00000011'), '00000010')
 
 
 if __name__ == "__main__":
