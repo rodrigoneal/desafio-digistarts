7d8cea8d154394b37edb109cbd173dcac9732440
rodrigoneal <rodrigho2006@gmail.com>
2021-01-15 19:36:36 -0300

    fix bugs

---
+++ desafio_conjunto/return_unique.py
@@ -0,0 +1,29 @@
+from typing import Iterable
+from random import randint
+
+'''
+Modo mais simples usando o set que não aceita valores repetitos
+'''
+
+
+def unique_with_set(cont: int) -> list[int]:
+    if isinstance(cont, int):
+        lista = [randint(1, 1000) for _ in range(cont)]
+        return sorted(set(lista))
+    else:
+        raise AttributeError('The attribute must be of type int')
+
+
+'''
+Modo mais trabalhando usando for e verificando se o valor está na lista
+'''
+
+
+def unique_with_for(cont: int) -> list[int]:
+    lista = [randint(-1000, 1000) for _ in range(cont)]
+    unique = list()
+    for i in lista:
+        if i not in unique:
+            unique.append(i)
+    unique.sort()
+    return unique
